10accf61f8b66cc29cbb92a6c1fa15ebabc8ccd8
Agsbangalore <agsbangalore07@gmail.com>
2016-06-22 09:31:21 -0700

    added flexslider to client section

---
+++ js/functions.js
@@ -0,0 +1,82 @@
+$(function() {
+
+
+
+
+      (function() {
+
+         // store the slider in a local variable
+         var $window = $(window),
+        flexslider = { vars:{} };
+
+         // tiny helper function to add breakpoints
+         function getGridSize() {
+      return (window.innerWidth < 600) ? 3 :       //default (2) no. of columns below 600px window width *******
+             (window.innerWidth < 900) ? 4 : 6;   //default (3:4) no. of columns below 900px window width *******
+         }
+
+
+         $window.load(function() {
+      $('.flexslider.clientBox').flexslider({
+        animation: "slide",
+        animationSpeed: 400,
+        animationLoop: false,
+        itemWidth: 210,
+        itemMargin: 0,                          //default itemMargin: 5 ******
+        minItems: getGridSize(), // use function to pull in initial value
+        maxItems: getGridSize(), // use function to pull in initial value
+        start: function(slider){
+          $('body').removeClass('loading');
+          flexslider = slider;
+        }
+      });
+         });
+
+         // check grid size on resize event
+         $window.resize(function() {
+      var gridSize = getGridSize();
+
+      flexslider.vars.minItems = gridSize;
+      flexslider.vars.maxItems = gridSize;
+         });
+      }());
+
+
+
+
+
+
+
+
+
+
+
+      $(window).load(function() {
+      $('.flexslider').flexslider();
+      });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
