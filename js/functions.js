b6ca30d1ac6d0ff5eb2cc2e832f574ceaca5bd16
Agsbangalore <agsbangalore07@gmail.com>
2016-08-26 03:06:34 -0700

    added clients, removed social section, added disclaimer, terms n conditions, privacy policy

--- js/functions.js
+++ js/functions.js
@@ -76,23 +76,21 @@ setTimeout(
 
 
 
-      	//Check to see if the window is top if not then display button
-      	$(window).scroll(function(){
-      		if ($(this).scrollTop() > 100) {
-      			$('.scrollToTop').fadeIn();
-      		} else {
-      			$('.scrollToTop').fadeOut();
-      		}
-      	});
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
 
 
-      	//Click event to scroll to top
-      	$('.scrollToTop').click(function(){
-      		// $('html, body').animate({scrollTop: 0},800);
-                  $('.pageContent').animate({scrollLeft:-50},500);
-                  $('.pageContent').animate({scrollTop:-50},500);
-      		return false;
-      	});
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop: 0},800);
+		return false;            
+	});
 
 
 
