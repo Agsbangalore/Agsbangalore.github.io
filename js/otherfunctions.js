f9a2d4671e69f542b3af7b8d852d4d9744b8c253
Agsbangalore <agsbangalore07@gmail.com>
2016-09-15 10:51:44 -0700

    IntroHead animation superfix

--- js/otherfunctions.js
+++ js/otherfunctions.js
@@ -7,33 +7,19 @@ if($(window).width()>1000){
 
             $(window).scroll(function(){
                   var wScroll = $(this).scrollTop(),
-                  introTop = $('.introSection').offset().top,
-                  windowHeight = $(window).height(),
                   introHeadHeight = $('.introHead').height(),
                   introHead = $('.introHead').offset().top,
-                  introHeadBottom = $('.introHeadFloat').offset().top + $('.introHeadFloat').height();
+                  introFloatBottom = $('.introHeadFloat').offset().top + $('.introHeadFloat').height(),
+                  introHeadBottom = introHead + introHeadHeight;
 
-                  // console.log("wScroll "+wScroll);
-                  // console.log("introTop "+introTop);
-                  // console.log("windowHeight "+windowHeight);
-                  // console.log("introHeadHeight "+introHeadHeight);
-                  // console.log("introHead "+introHead);
-                  // console.log("introHeadBottom "+introHeadBottom);
-                  // console.log("==================");
 
-                  if(((wScroll+windowHeight)>=introHeadBottom+1)&&
-                  ((wScroll+windowHeight)<=introHeadBottom+((introHeadHeight)/3))){
+            if(introFloatBottom >= introHeadBottom){
+                  $('.introHead').css({
+                        'transform' : 'translate(0px, '+ (Math.min(wScroll/30, 12.5)) +'vw)'
+                  });
+            }
 
-                        $('.introHead').css({
-                        'transform' : 'translate(0px, '+ (-12.5+((wScroll+windowHeight)-introHeadBottom))/8 +'vw)'
-                        });
 
-                  }else if ((wScroll+windowHeight)>introHeadBottom) {
-
-                        $('.introHead').css({
-                        'transform' : 'translate(0px, 12.5vw)'
-                        });
-                  }
 
             });
 
