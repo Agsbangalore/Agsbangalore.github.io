5e2ca7194519b2025bda3d8ce57e260db5378372
Agsbangalore <agsbangalore07@gmail.com>
2016-10-03 05:36:44 -0700

    changes to footer styling, tiles text and introText of sub pages

--- js/otherfunctions.js
+++ js/otherfunctions.js
@@ -1,30 +1,30 @@
-$(function() {
-
-
-if($(window).width()>1000){
-
-/*child pages title animation*/
-
-            $(window).scroll(function(){
-                  var wScroll = $(this).scrollTop(),
-                  introHeadHeight = $('.introHead').height(),
-                  introHead = $('.introHead').offset().top,
-                  introFloatBottom = $('.introHeadFloat').offset().top + $('.introHeadFloat').height(),
-                  introHeadBottom = introHead + introHeadHeight;
-
-
-            if(introFloatBottom >= introHeadBottom){
-                  $('.introHead').css({
-                        'transform' : 'translate(0px, '+ (Math.min(wScroll/30, 12.5)) +'vw)'
-                  });
-            }
-
-
-
-            });
-
-}
-
+// $(function() {
+//
+//
+// if($(window).width()>1000){
+//
+// /*child pages title animation*/
+//
+//             $(window).scroll(function(){
+//                   var wScroll = $(this).scrollTop(),
+//                   introHeadHeight = $('.introHead').height(),
+//                   introHead = $('.introHead').offset().top,
+//                   introFloatBottom = $('.introHeadFloat').offset().top + $('.introHeadFloat').height(),
+//                   introHeadBottom = introHead + introHeadHeight;
+//
+//
+//             if(introFloatBottom >= introHeadBottom){
+//                   $('.introHead').css({
+//                         'transform' : 'translate(0px, '+ (Math.min(wScroll/30, 12.5)) +'vw)'
+//                   });
+//             }
+//
+//
+//
+//             });
+//
+// }
+//
 
 
 
